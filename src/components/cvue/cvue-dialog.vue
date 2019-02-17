34e71d3085559a0606a00ed21c4bd62922ec5178
chenyong <yong.chen@doone.com.cn>
2019-02-17 09:28:46 +0800

    添加公共组件

---
+++ src/components/cvue/cvue-dialog.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'cvueDialog',
+    data () {
+        return {
+            msg: 'hello'
+        }
+    }
+}
+</script>
+<style lange = "scss" scoped>
+</style>
